3b2c684bfbb14e5ff0992bc6fecf62bdf50a05d5
sahilverma817 <sahilverma817@gmail.com>
2023-01-26 22:05:21 +0530

    Managing frontent authentication

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,121 +1,149 @@
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
-import Grid from '@material-ui/core/Grid';
-import '../styles/login.css';
-import IITGoaLogo from '../images/iit_goa_logo.svg';
-import zIndex from '@material-ui/core/styles/zIndex';
+import React, { useState } from "react";
+import { Form, Input, Button } from "antd";
+import Grid from "@material-ui/core/Grid";
+import "../styles/login.css";
+import IITGoaLogo from "../images/iit_goa_logo.svg";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    
-    //integration with backend
-    function handleSubmit(e) {
-        e.preventDefault();
-    }
-    
-    return (
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    <div className="login-page" 
+  //integration with backend
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Handle submit called");
+    //Sending username & password in json format to  http://127.0.0.1:5000 and recieve a jwt token.
+    fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }).then((response) => {
+      console.log(response);
+      if (response.status === 200) {
+        //redirect to home page and set jwt token in local storage
+        localStorage.setItem("Authorization", response.access_token);
+        window.location.href = "/";
+      } else {
+        setError("Incorrect username or password");
+      }
+    });
+  }
+
+  return (
+    <div
+      className="login-page"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#e5e5e5",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div
+        className="login-card"
         style={{
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center',
-            backgroundColor: '#e5e5e5',
-            height: '100vh',
-            width: '100vw'
-        }}>
-        <div 
-            className='login-card'
-            style={{
-                marginTop: '4rem',
-                borderStyle: 'solid',
-                borderWidth: '1px',
-                borderRadius: '10px',
-                borderColor: '#C8C8C8',
-                height: '70vh',
-                width: '70vw',
-            }}
-        >
-
+          marginTop: "4rem",
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderRadius: "10px",
+          borderColor: "#C8C8C8",
+          height: "70vh",
+          width: "70vw",
+        }}
+      >
         <Grid
-        container
-        // spacing={2}
-        direction="row"
-        alignItems="center"
-        justifyContent="center"
-        style={{ height: '100%' }}
+          container
+          // spacing={2}
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          style={{ height: "100%" }}
         >
-
-        <Grid item xs={6} 
+          <Grid
+            item
+            xs={6}
             style={{
-                height: '100%',
-                width: '100%',
-                backgroundColor: 'white',
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center',
-                borderRadius: '10px 0px 0px 10px'
-            }}>
-            <img src={IITGoaLogo} alt="IIT Goa Logo" width='60%' height='60%' />
-        </Grid>  
+              height: "100%",
+              width: "100%",
+              backgroundColor: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "10px 0px 0px 10px",
+            }}
+          >
+            <img src={IITGoaLogo} alt="IIT Goa Logo" width="60%" height="60%" />
+          </Grid>
 
-        <Grid item xs={6}
+          <Grid
+            item
+            xs={6}
             style={{
-                
-                backgroundColor: '#a5bfd7',
-                height: '100%',
-                width: '100%',
-                display: 'flex', 
-                justifyContent: 'center',
-                alignItems: 'center',
-                borderRadius: '0px 10px 10px 0px',
-            }}>
+              backgroundColor: "#a5bfd7",
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "0px 10px 10px 0px",
+            }}
+          >
             <div style={{}}>
-                <h2 className="text-center"
+              <h2
+                className="text-center"
+                style={{
+                  fontFamily: "Poppins",
+                  color: "#00264d",
+                }}
+              >
+                Login
+              </h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Item>
+                  <Input
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Item>
+                {error && <div className="error">{error}</div>}
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
                     style={{
-                        fontFamily: 'Poppins',
-                        color: '#00264d',
-                    }}> 
+                      backgroundColor: "#00264d",
+                      borderRadius: "15px",
+                      width: "60%",
+                      height: "100%",
+                      fontFamily: "Poppins",
+                    }}
+                  >
                     Login
-                </h2>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Item>
-                        <Input
-                            placeholder="Username"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </Form.Item>
-                    {error && <div className="error">{error}</div>}
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit"
-                            style={{
-                                backgroundColor: '#00264d',
-                                borderRadius: '15px',
-                                width: '60%',
-                                height: '100%',
-                                fontFamily: 'Poppins'
-                            }}>
-                            Login
-                        </Button>
-                    </Form.Item>
-                </Form>
+                  </Button>
+                </Form.Item>
+              </Form>
             </div>
-        </Grid>  
+          </Grid>
         </Grid>
-
-        
-        </div>
+      </div>
     </div>
   );
 }
