545004e40c66b51d99f617db62d0e2480591f43b
Khushboo Gupta <khushboo@persona3.io>
2023-02-03 02:35:54 +0530

    added routing and grades page

--- frontend/src/pages/grades.js
+++ frontend/src/pages/grades.js
@@ -1,43 +1,200 @@
-import React from 'react';
-import DisplayRow from '../components/displayRow.js';
+import React from "react";
+import { 
+  Avatar, 
+  Table, 
+  TableBody,
+  TableCell, 
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography } from "@material-ui/core";
 
-const GradesPage = () => {
-  const data = [
+
+function GradesPage(){
+
+  const rows = [
     {
-      semester: 'SPRING 2022-23',
-      text1: 'Text 1',
-      text2: 'Text 2',
-      text3: 'Text 3',
-      text4: 'Text 4',
-      text5: 'Text 5',
-      text6: 'Text 6',
+      'course' : 'Course Code 1',
+      'courseTitle' : 'Course Name 1',
+      'credits' : 'Course Credits 1',
+      'registrationType': 'Registration Type 1',
+      'electiveType' : 'Elective Type 1',
+      'grade' : 'Grade 1'
     },
     {
-      text1: 'Text 1',
-      text2: 'Text 2',
-      text3: 'Text 3',
-      text4: 'Text 4',
-      text5: 'Text 5',
-      text6: 'Text 6',
-    },
+      'course' : 'Course Code 2',
+      'courseTitle' : 'Course Name 2',
+      'credits' : 'Course Credits 2',
+      'registrationType': 'Registration Type 2',
+      'electiveType' : 'Elective Type 2',
+      'grade' : 'Grade 2'
+    }
   ];
 
-  return (
-    <div>
-      <DisplayRow text1={data[0][0]}>
-      {...data.map((item, index) => (
-        <DisplayRow
-          key={index}
-          text1={item.text1}
-          text2={item.text2}
-          text3={item.text3}
-          text4={item.text4}
-          text5={item.text5}
-          text6={item.text6}
-        />
-      ))}
+  return(
+    <div
+      className="login-page"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#e5e5e5",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <div
+        className="main-profile-table"
+        style={{
+          backgroundColor: "white",
+          height: '80%',
+          width: '90%',
+          borderRadius: '10px'
+        }}>
+
+          <div 
+            className="student-details"
+            style={{
+              backgroundColor: "#a5bfd7",
+              height: '25%',
+              marginTop: '1rem',
+              marginBottom: '2rem',
+            }}>
+              <div style={{
+                marginLeft: '1rem',
+                display: 'flex',
+                height: '100%',
+                width: '100%'
+              }}>
+                <div style={{
+                  marginTop: '1rem'
+                }}>
+                  <Avatar style={{
+                    width: 130,
+                    height: 130
+                  }}>
+                    SN
+                  </Avatar>
+                </div>
+                <div style={{
+                  marginLeft: '2.5rem',
+                  marginTop: '1rem',
+                  width: '100% !important'
+                }}>
+                  <Typography style={{
+                    fontFamily: "Poppins",
+                    color: "#00264d",
+                    fontSize: 30
+                  }}>
+                    Student Name
+                  </Typography>
+                  <Typography style={{
+                    fontFamily: "Poppins",
+                    color: "#00264d",
+                    fontSize: 16,
+                  }}>
+                    Roll Number: 
+                  </Typography>
+                  <Typography style={{
+                    fontFamily: "Poppins",
+                    color: "#00264d",
+                    fontSize: 16,
+                  }}>
+                    Branch Name: 
+                  </Typography>
+                  <Typography style={{
+                    fontFamily: "Poppins",
+                    color: "#00264d",
+                    fontSize: 16,
+                  }}>
+                    Student Type:
+                  </Typography>
+
+                  
+                </div>
+              </div>
+              
+          
+
+          </div>
+
+          <div className="grades-table">
+          <TableContainer>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow style={{
+                    backgroundColor: '#00264d',
+                  }}>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="center">
+                      Course
+                    </TableCell>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="right">Course Title</TableCell>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="right">Credits</TableCell>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="right">Registration Type</TableCell>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="right">Elective Type</TableCell>
+                    <TableCell style={{
+                      fontFamily: 'Poppins',
+                      color: 'white',
+                    }} align="right">Grade</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }} component="th" scope="row">
+                        {row.course}
+                      </TableCell>
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }}align="right">{row.courseTitle}</TableCell>
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }}align="right">{row.credits}</TableCell>
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }}align="right">{row.registrationType}</TableCell>
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }}align="right">{row.electiveType}</TableCell>
+                      <TableCell style={{
+                        fontFamily: 'Poppins',
+                        borderBottom: '1px solid #00264d',
+                      }}align="right">{row.grade}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default GradesPage;
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,224 +1,156 @@
 import React, { useState } from "react";
 import { Form, Input, Button } from "antd";
 import Grid from "@material-ui/core/Grid";
+import { useNavigate } from "react-router-dom";
 import "../styles/login.css";
 import IITGoaLogo from "../images/iit_goa_logo.svg";
-import zIndex from "@material-ui/core/styles/zIndex";
 
 function LoginPage() {
-<<<<<<< HEAD
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
-  //integration with backend
-  function handleSubmit(e) {
-    console.log('zzz');
-    e.preventDefault();
-=======
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
 
-  //integration with backend
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("Handle submit called");
-    //Sending username & password in json format to  http://127.0.0.1:5000 and recieve a jwt token.
-    fetch("http://127.0.0.1:5000/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-    }).then((response) => {
-      console.log(response);
-      if (response.status === 200) {
-        //redirect to home page and set jwt token in local storage
-        localStorage.setItem("Authorization", response.access_token);
-        window.location.href = "/";
-      } else {
-        setError("Incorrect username or password");
-      }
-    });
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
-  }
+    //integration with backend
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("Handle submit called");
+        
 
-  return (
-    <div
-      className="login-page"
-      style={{
-<<<<<<< HEAD
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#e5e5e5',
-        height: '100vh',
-        width: '100vw',
-=======
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#e5e5e5",
-        height: "100vh",
-        width: "100vw",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
-      }}
-    >
-      <div
-        className="login-card"
+        //Sending username & password in json format to  http://127.0.0.1:5000 and recieve a jwt token.
+        fetch("http://127.0.0.1:5000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+        }),
+        }).then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+            //redirect to home page and set jwt token in local storage
+            localStorage.setItem("Authorization", response.access_token);
+            navigate("/viewgrades");
+        } else {
+            setError("Incorrect username or password");
+        }
+        });
+    }
+
+    return (
+        <div
+        className="login-page"
         style={{
-<<<<<<< HEAD
-          marginTop: '4rem',
-          borderStyle: 'solid',
-          borderWidth: '1px',
-          borderColor: '#C8C8C8',
-          borderRadius: '10px 10px 10px 10px',
-          height: '70vh',
-          width: '70vw',
-=======
-          marginTop: "4rem",
-          borderStyle: "solid",
-          borderWidth: "1px",
-          borderRadius: "10px",
-          borderColor: "#C8C8C8",
-          height: "70vh",
-          width: "70vw",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#e5e5e5",
+            height: "100vh",
+            width: "100vw",
         }}
-      >
-        <Grid
-          container
-          // spacing={2}
-          direction="row"
-          alignItems="center"
-          justifyContent="center"
-<<<<<<< HEAD
-          style={{ height: '100%' }}
-=======
-          style={{ height: "100%" }}
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
         >
-          <Grid
-            item
-            xs={6}
+        <div
+            className="login-card"
             style={{
-<<<<<<< HEAD
-              height: '100%',
-              width: '100%',
-              backgroundColor: 'white',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: '10px 0px 0px 10px',
-=======
-              height: "100%",
-              width: "100%",
-              backgroundColor: "white",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "10px 0px 0px 10px",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
+            marginTop: "4rem",
+            borderStyle: "solid",
+            borderWidth: "1px",
+            borderRadius: "10px",
+            borderColor: "#C8C8C8",
+            height: "70vh",
+            width: "70vw",
             }}
-          >
-            <img src={IITGoaLogo} alt="IIT Goa Logo" width="60%" height="60%" />
-          </Grid>
-
-          <Grid
-            item
-            xs={6}
-            style={{
-<<<<<<< HEAD
-              backgroundColor: '#a5bfd7',
-              height: '100%',
-              width: '100%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: '0px 10px 10px 0px',
-=======
-              backgroundColor: "#a5bfd7",
-              height: "100%",
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "0px 10px 10px 0px",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
-            }}
-          >
-            <div style={{}}>
-              <h2
-                className="text-center"
+        >
+            <Grid
+            container
+            // spacing={2}
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            style={{ height: "100%" }}
+            >
+            <Grid
+                item
+                xs={6}
                 style={{
-<<<<<<< HEAD
-                  fontFamily: 'Poppins',
-                  color: '#00264d',
-=======
-                  fontFamily: "Poppins",
-                  color: "#00264d",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
+                height: "100%",
+                width: "100%",
+                backgroundColor: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "10px 0px 0px 10px",
                 }}
-              >
-                Login
-              </h2>
-<<<<<<< HEAD
-              <Form>
-=======
-              <Form onSubmit={handleSubmit}>
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
-                <Form.Item>
-                  <Input
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </Form.Item>
-                <Form.Item>
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Item>
-                {error && <div className="error">{error}</div>}
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-<<<<<<< HEAD
-                    onSubmit={handleSubmit}
+            >
+                <img src={IITGoaLogo} alt="IIT Goa Logo" width="60%" height="60%" />
+            </Grid>
+
+            <Grid
+                item
+                xs={6}
+                style={{
+                backgroundColor: "#a5bfd7",
+                height: "100%",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "0px 10px 10px 0px",
+                }}
+            >
+                <div style={{}}>
+                <h2
+                    className="text-center"
                     style={{
-                      backgroundColor: '#00264d',
-                      borderRadius: '15px',
-                      width: '60%',
-                      height: '100%',
-                      fontFamily: 'Poppins',
-=======
-                    style={{
-                      backgroundColor: "#00264d",
-                      borderRadius: "15px",
-                      width: "60%",
-                      height: "100%",
-                      fontFamily: "Poppins",
->>>>>>> 36afe077ce6281f026384d40a76da7fe3ab3ab21
+                    fontFamily: "Poppins",
+                    color: "#00264d",
                     }}
-                  >
+                >
                     Login
-                  </Button>
-                </Form.Item>
-              </Form>
-            </div>
-          </Grid>
-        </Grid>
-      </div>
-    </div>
-  );
+                </h2>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Item>
+                    <Input
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    </Form.Item>
+                    <Form.Item>
+                    <Input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    </Form.Item>
+                    {error && <div className="error" style={{marginBottom: '1rem'}}>{error}</div>}
+                    <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        style={{
+                        backgroundColor: "#00264d",
+                        borderRadius: "15px",
+                        width: "60%",
+                        height: "100%",
+                        fontFamily: "Poppins",
+                        }}
+                        onClick={handleSubmit}
+                    >
+                        Login
+                    </Button>
+                    </Form.Item>
+                </Form>
+                </div>
+            </Grid>
+            </Grid>
+        </div>
+        </div>
+    );
 }
 
 export default LoginPage;
