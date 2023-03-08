0604d4d2b0873be01a352568dc77f185893ee1f6
RaHack238 <raghvendra.singh.19031@iitgoa.ac.in>
2023-03-09 00:50:40 +0530

    Models uploaded

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
 
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-from app import routes
+from app import routes, models
 
 
