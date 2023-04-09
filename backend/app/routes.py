f4cc7b41047b8e95b30d0bb0c68c6c976090daa1
Sahil Verma <sahilverma817@gmail.com>
2023-04-09 13:57:44 +0530

    Updated viewGrades endpoint

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,16 +1,18 @@
 from app import app, jwt, db
 from flask import request, jsonify
 from functools import wraps
-from app.models import Student, Teacher, Grade, Course, Classroom
+from app.models import Student, Teacher, Grade, Course, Classroom, ICard, ScholarshipDB
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 import json 
 from collections import defaultdict
+from datetime import datetime
+
 
 @app.route('/')
 def home():
     return "Welcome To IIT Goa Ams Poral"
 
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 @jwt_required()
 def index():
     user=get_jwt_identity()
@@ -32,6 +34,7 @@ def login():
     else:
         return jsonify(message="Bad username or password"), 401
 
+
 @app.route('/viewGrades', methods=['GET'])
 @jwt_required()
 def viewGrades():
@@ -50,17 +53,12 @@ def viewGrades():
                     .all()
 
         # Group the grades by semester
-        semesters = defaultdict(list)
+        semesters = defaultdict(dict)
         for grade in grades:
-            semesters[grade.sem].append(grade._asdict())
-            
+            semesters[f'sem{grade.sem}'][grade.course_name] = grade._asdict()
+            semesters[f'sem{grade.sem}'][grade.course_name].pop('course_name')
         
-        # Format the grades as a dictionary
-        grade_dict = {}
-        for sem, grades in semesters.items():
-            grade_dict[f'sem{sem}'] = grades
-
-        return jsonify(user=user, grades=grade_dict), 200
+        return jsonify(user=user, grades=semesters), 200
 
     except Exception as e:
         return jsonify(message=str(e)), 500
@@ -93,9 +91,87 @@ def viewAllStudents():
         return jsonify(students=student_list), 200
     except Exception as e:
         return jsonify(message=str(e)), 500
+
+
+@app.route('/icardSubmit', methods=['POST'])
+@jwt_required()
+def icardSubmit():
+    try:
+        student_id=get_jwt_identity()
+        data=request.get_json()
+        sign=data['sign']
+        image=data['image']
+        icard=ICard(student_id=student_id, sign=sign, image=image)
+        db.session.add(icard)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(message=str(e)), 500
     
 
 
+@app.route('/submitScholarship', methods=['POST'])
+@jwt_required()
+def submitScholarship():
+    try:
+        student_id=get_jwt_identity()
+        data=request.get_json()
+
+        # Parse date strings into datetime objects and format as required
+        otherAppliedScholarshipsStartDate = datetime.strptime(data['otherAppliedScholarshipsStartDate'], '%Y-%m-%d').strftime('%d-%m-%Y')
+        otherAppliedScholarshipsEndDate = datetime.strptime(data['otherAppliedScholarshipsEndDate'], '%Y-%m-%d').strftime('%d-%m-%Y')
+        ongoingScholarshipsStartDate = datetime.strptime(data['ongoingScholarshipsStartDate'], '%Y-%m-%d').strftime('%d-%m-%Y')
+        ongoingScholarshipsEndDate = datetime.strptime(data['ongoingScholarshipsEndDate'], '%Y-%m-%d').strftime('%d-%m-%Y')
+
+        # Create ScholarshipDB object with formatted dates
+        scholarship_db = ScholarshipDB(
+            student_id=student_id,
+            otherAppliedScholarships=data['otherAppliedScholarships'],
+            otherAppliedScholarshipsAmount=data['otherAppliedScholarshipsAmount'],
+            otherAppliedScholarshipsOrganization=data['otherAppliedScholarshipsOrganization'],
+            otherAppliedScholarshipsStartDate=otherAppliedScholarshipsStartDate,
+            otherAppliedScholarshipsEndDate=otherAppliedScholarshipsEndDate,
+            ongoingScholarships=data['ongoingScholarships'],
+            ongoingScholarshipsAmount=data['ongoingScholarshipsAmount'],
+            ongoingScholarshipsOrganization=data['ongoingScholarshipsOrganization'],
+            ongoingScholarshipsStartDate=ongoingScholarshipsStartDate,
+            ongoingScholarshipsEndDate=ongoingScholarshipsEndDate,
+            bankName=data['bankName'],
+            bankAccountNumber=data['bankAccountNumber'],
+            bankIFSC=data['bankIFSC'],
+            bankBranch=data['bankBranch'],
+            fatherOccupation=data['fatherOccupation'],
+            fatherIncome=data['fatherIncome'],
+            motherOccupation=data['motherOccupation'],
+            motherIncome=data['motherIncome'],
+            guardianOccupation=data['guardianOccupation'],
+            guardianIncome=data['guardianIncome']
+        )
+
+        db.session.add(scholarship_db)
+        db.session.commit()
+        return jsonify({'message': 'Scholarship submitted successfully'}), 200
+
+    except Exception as e:
+        print(str(e))
+        return jsonify({'message': 'Something went wrong'}), 500
+
+        
+
+
+@app.route('/viewScholarship', methods=['GET'])
+@jwt_required()
+def viewScholarship():
+    try:
+        student_id=get_jwt_identity()
+        scholarship = ScholarshipDB.query.filter_by(student_id=student_id).first()
+        if scholarship:
+            scholarship_dict = scholarship.as_dict()
+            return jsonify(scholarship=scholarship_dict), 200
+        else:
+            return jsonify(message='No scholarship found'), 404
+
+    except Exception as e:
+        return jsonify(message=str(e)), 500
 
 
 
