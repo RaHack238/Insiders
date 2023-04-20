5cc677d0f7f4d50ac82c19e3ede26afaf1a9b41b
Aryan Yadav <akshayj00001@gmail.com>
2023-04-20 16:46:46 +0530

    added logout btn

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -5,16 +5,22 @@ import Requestidbutton from './buttons/requestid';
 import Amsbutton from './buttons/ams';
 import Scholarshipbutton from './buttons/scholarshipapply';
 import CourseRegistrationbutton from './buttons/courseregistration';
+import Logoutbutton from './buttons/logout';
 
 const Navbar = () => (
     <header className='navbar'>
         {/* <div className='navbar__item navbar__title'>AMS</div> */}
         {/* <div className='navbar__item'>Request i-Card</div> */}
-        <Amsbutton />
-        <Requestidbutton />
-        <Mydetailsbutton />    
-        <Scholarshipbutton />
-        <CourseRegistrationbutton />
+            <Amsbutton />
+            <Requestidbutton />
+            <Mydetailsbutton />    
+            <Scholarshipbutton />
+            <CourseRegistrationbutton />
+        
+        <div className='navbar__end'>
+            <Logoutbutton />
+        </div>
+        
     </header>
 );
 
